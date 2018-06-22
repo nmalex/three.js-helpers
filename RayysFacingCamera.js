69894f39d3f15e5e6ad17d7cf110a7ac279a3c0f
nmalex <horoshiloff@gmail.com>
2018-06-22 10:31:35 +0200

    collect and notify all facing directions

--- RayysFacingCamera.js
+++ RayysFacingCamera.js
@@ -6,7 +6,7 @@ class RayysFacingCamera {
     // camera looking direction will be saved here
     this.dirVector = new THREE.Vector3();
 
-		// all world directions
+    // all world directions
     this.dirs = [
       new THREE.Vector3(+1, 0, 0),
       new THREE.Vector3(-1, 0, 0),
@@ -16,12 +16,13 @@ class RayysFacingCamera {
       new THREE.Vector3(0, 0, -1)
     ];
 
-		// index of best facing direction will be saved here
-    this.facingDir = undefined;
+    // index of best facing direction will be saved here
+    this.facingDirs = [];
+    this.bestFacingDir = undefined;
     
     // TODO: add other facing directions too
 
-		// event listeners are collected here
+    // event listeners are collected here
     this.cb = {
       facingDirChange: []
     };
@@ -34,32 +35,35 @@ class RayysFacingCamera {
     var maxk = 0;
     var maxdot = -1e19;
 
+    var oldFacingDirs = this.facingDirs;
+    var facingDirsChanged = false;
+    this.facingDirs = [];
+	
     for (var k = 0; k < this.dirs.length; k++) {
       var dot = this.dirs[k].dot(this.dirVector);
       if (dot > -Math.PI / 2 && dot < Math.PI / 2) {
-        var adot = Math.abs(dot);
-        if (adot > maxdot) {
+	this.facingDirs.push(k);
+	if (oldFacingDirs.indexOf(k) === -1) {
+	  facingDirsChanged = true;
+	}
+        if (Math.abs(dot) > maxdot) {
           maxdot = dot;
           maxk = k;
         }
       }
     }
 
-		// and if facing direction changed, notify subscribers
-    if (maxk !== this.facingDir) {
-      var prevDir = this.facingDir;
-      this.facingDir = maxk;
+    // and if facing direction changed, notify subscribers
+    if (maxk !== this.bestFacingDir || facingDirsChanged) {
+      var prevDir = this.bestFacingDir;
+      this.bestFacingDir = maxk;
 
       for (var i = 0; i < this.cb.facingDirChange.length; i++) {
         this.cb.facingDirChange[i]({
-          before: prevDir,
-          current: this.facingDir
-        });
+          before: { facing: oldFacingDirs, best: prevDir },
+          current: { facing: this.facingDirs, best: this.bestFacingDir }
+        }, this);
       }
     }
-  }
-
-  subscribe(onFacingDirChange) {
-    this.cb.facingDirChange.push(onFacingDirChange);
   }
 }
