4c3c93498756095ebe4b37a1cbc6f922e1967184
alex.khoroshylov <alex.khoroshylov@avira.com>
2018-06-29 13:52:04 +0200

    implemented BBoxSnapFactory, adjusted classes for flexible and reusable snap logic

--- RayysSnap.js
+++ RayysSnap.js
@@ -1,19 +1,223 @@
+// snap target may be of 1 of 3 types:
+// 1. Vector3 - point in world space
+// 2. Line - line in world space
+// 3. Plane - plane in world space
+class SnapTarget {
+    constructor() {
+        this.points = [];
+        this.lines = [];
+        this.planes = [];
+    }
+
+    // try to snap this targets to given snap target, using given snap actor
+    // and if it snaps - return Vector3 of the desired snap object offset AND snapped point, line or plane
+    snap(other, actor, isTwoSided) {
+
+    }
+
+    getNode() {
+        // todo: create scene node to visualize snap points, lines and planes
+    }
+}
+
+// SnapMaker will create a snap target from given object. Different factories 
+// may return very specific collections of snap targets.
+class BBoxSnapFactory {
+    constructor() {
+    }
+
+    // creates snap target for given scene object
+    create(obj, snapToPivot, snapToPoints, snapToLines, snapToPlanes, snapToMidlines, snapToMidplanes) {
+        var snapTarget = new SnapTarget();
+        update(obj, snapTarget, snapToPivot, snapToPoints, snapToLines, snapToPlanes, snapToMidlines, snapToMidplanes);
+        return snapTarget;
+    }
+
+    update(obj, snapTargetRef, snapToPivot, snapToPoints, snapToLines, snapToPlanes, snapToMidlines, snapToMidplanes) {
+        if (obj.boundingBox === undefined) {
+            obj.computeBoundingBox();
+        }
+        var bbox = obj.boundingBox;
+
+        snapTargetRef.points.length = 0;
+        snapTargetRef.lines.length = 0;
+        snapTargetRef.planes.length = 0;
+
+        var points = snapTargetRef.points;
+        if (snapToPivot) {
+            points.push(obj.position.clone());
+        }
+
+        if (snapToPoints) {
+            points.push(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z));
+            points.push(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z));
+
+            points.push(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z));
+            points.push(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z));
+
+            points.push(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z));
+            points.push(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z));
+
+            points.push(new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z));
+            points.push(new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z));
+        }
+
+        var lines = snapTargetRef.lines;
+        if (snapToLines) {
+            // x-axis oriented lines
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+
+            // y-axis oriented lines
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+
+            // z-axis oriented lines
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z)));
+            lines.push(new THREE.Line3(new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+        }
+
+        var planes = snapTargetRef.planes;
+        if (snapToPlanes) {
+            var planeXZPositive = new THREE.Plane();
+            planeXZPositive.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,1,0), bbox.max );
+            planes.push(planeXZPositive);
+
+            var planeXZNegative = new THREE.Plane();
+            planeXZNegative.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,-1,0), bbox.min );
+            planes.push(planeXZNegative);
+
+            var planeXYPositive = new THREE.Plane();
+            planeXYPositive.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,0,1), bbox.max );
+            planes.push(planeXYPositive);
+
+            var planeXYNegative = new THREE.Plane();
+            planeXYNegative.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,0,-1), bbox.min );
+            planes.push(planeXYNegative);
+
+            var planeYZPositive = new THREE.Plane();
+            planeYZPositive.setFromNormalAndCoplanarPoint ( new THREE.Vector3(1,0,0), bbox.max );
+            planes.push(planeYZPositive);
+
+            var planeYZNegative = new THREE.Plane();
+            planeYZNegative.setFromNormalAndCoplanarPoint ( new THREE.Vector3(-1,0,0), bbox.min );
+            planes.push(planeYZNegative);
+        }
+
+        var getMidline = function(p0, p1) {
+            return (p0.clone()).add(p1).divideScalar ( 2.0 );
+        };
+        if (snapToMidlines) {
+            // x-axis oriented midline points
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+
+            // y-axis oriented midline points
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+
+            // z-axis oriented midline points
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z)));
+            points.push(getMidline(new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z), new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z)));
+        }
+
+        var getMidplane = function(p0, p1, p2, p3) {
+            return (p0.clone()).add(p1).add(p2).add(p3).divideScalar ( 4.0 );
+        }
+        if (snapToMidplanes) {
+            // x is fixed
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), 
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), 
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z), 
+            ));
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z), 
+            ));
+
+            // y is fixed
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), 
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z), 
+            ));
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), 
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z), 
+            ));
+
+            // z is fixed
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.min.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.min.z), 
+            ));
+            points.push(getMidplane(
+                new THREE.Vector3(bbox.min.x, bbox.min.y, bbox.max.z), 
+                new THREE.Vector3(bbox.min.x, bbox.max.y, bbox.max.z), 
+                new THREE.Vector3(bbox.max.x, bbox.min.y, bbox.max.z), 
+                new THREE.Vector3(bbox.max.x, bbox.max.y, bbox.max.z), 
+            ));
+        }
+    }
+}
+
 class RayysSnap {
     constructor(threshold) {
         this.threshold = threshold;
+
+        // which snap actors you want to use?
         this.actors = [ ];
-        this.targets = [ ];
+
+        // which objects you want to snap to each other?
+        this.objects = [ ];
+        // this is a collection of id => targets, where as id is scene object id, and targets are this object's targets
+        this.targets = { };
+
+        // which snap factory should be used to generate targets?
+        this.factory = new BBoxSnapFactory();
     }
 
-    snap(obj, previewValue) {
+    // this will return snapped object position for the object being manipulated and raw object position
+    snap(obj, previewPos) {
+
+    }
+
+    // todo: rework it
+    snap(objectSnaps, previewValue) {
         for (let i=0; i<this.actors.length; i++) {
             let actor = this.actors[i];
-            let res = actor.snap( obj, previewValue, undefined, this.threshold );
+
+            // try to snap to the world, here procedural snaps may apply, 
+            // like grid snap which does not require specific targets
+            let res = actor.snap( objectSnaps, previewValue, undefined, this.threshold );
             if (res !== false) {
                 return res;
             }
+
+            // next try to check given snaps with existing snap targets
             for (let j=0; j<this.targets.length; j++) {
-                let res = actor.snap( obj, previewValue, this.targets[j], this.threshold );
+                let res = actor.snap( objectSnaps, previewValue, this.targets[j], this.threshold );
                 if (res !== false) {
                     return res;
                 }
@@ -23,7 +227,7 @@ class RayysSnap {
     }
 }
 
-class RayysPos3DSnapActor {
+class RayysPointSnapActor {
     snap( obj, previewPos, targetPos, threshold ) {
         if (previewPos.distanceTo(targetPos) < threshold) {
             return targetPos;
@@ -32,11 +236,20 @@ class RayysPos3DSnapActor {
     }
 }
 
+class RayysLineSnapActor {
+
+}
+
+class RayysPlaneSnapActor {
+
+}
+
 class RayysGridSnapActor {
     constructor(dx, dy, dz) {
         this.dx = dx;
         this.dy = dy;
         this.dz = dz;
+        this.isProcedural = true;
     }
     snap( obj, previewPos, targetPos, threshold ) {
         let x = Math.floor(previewPos.x / this.dx) * this.dx;
---
+++ src/rotation-gizmo.js
@@ -0,0 +1,352 @@
+var renderer;
+var controls;
+
+var scene = new THREE.Scene();
+var width = window.innerWidth;
+var height = window.innerHeight;
+var aspect = width / height;
+var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+
+renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(new THREE.Color(0x303030));
+document.body.appendChild(renderer.domElement);
+
+camera.position.x = 5;
+camera.position.y = 5;
+camera.position.z = 5;
+camera.lookAt(0, 0, 0);
+
+controls = new THREE.OrbitControls(camera);
+
+// white spotlight shining from the side, casting a shadow
+var spotLight = new THREE.SpotLight(0xffffff, 2.5, 25, Math.PI / 6);
+spotLight.position.set(4, 10, 1);
+scene.add(spotLight);
+
+// grid
+var size = 10;
+var divisions = 10;
+var gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+var origin = new THREE.Vector3(0, 0, 0);
+var radius = 2;
+
+var gizmo = new THREE.Object3D();
+
+var geometry = new THREE.SphereBufferGeometry( radius-1e-1, 32, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0x00000, transparent: true, opacity: 0.02} );
+var sphere = new THREE.Mesh( geometry, material );
+sphere.layers.set(2);
+gizmo.add( sphere );
+
+var gizmoCircles = [ sphere ];
+
+// X-plane curve
+var curve = new THREE.EllipseCurve(
+	0,  0,            // ax, aY
+	radius, radius,   // xRadius, yRadius
+	0,  2 * Math.PI,  // aStartAngle, aEndAngle
+	false,            // aClockwise
+	0                 // aRotation
+);
+
+var points = curve.getPoints( 90 );
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+var ellipseX = new THREE.Line( geometry, material );
+gizmoCircles.push(ellipseX);
+ellipseX.applyMatrix(new THREE.Matrix4().makeRotationX(Math.PI/2));
+ellipseX.layers.set(1);
+gizmo.add(ellipseX);
+
+// Y-plane curve
+var curve = new THREE.EllipseCurve(
+	0,  0,            // ax, aY
+	radius, radius,   // xRadius, yRadius
+	0,  2 * Math.PI,  // aStartAngle, aEndAngle
+	false,            // aClockwise
+	0                 // aRotation
+);
+
+var points = curve.getPoints( 90 );
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var material = new THREE.LineBasicMaterial( { color : 0x00ff00 } );
+var ellipseY = new THREE.Line( geometry, material );
+gizmoCircles.push(ellipseY);
+ellipseY.layers.set(1);
+gizmo.add(ellipseY);
+
+// Z-plane curve
+var curve = new THREE.EllipseCurve(
+	0,  0,            // ax, aY
+	radius, radius,   // xRadius, yRadius
+	0,  2 * Math.PI,  // aStartAngle, aEndAngle
+	false,            // aClockwise
+	0                 // aRotation
+);
+
+var points = curve.getPoints( 90 );
+var geometry = new THREE.BufferGeometry().setFromPoints( points );
+var material = new THREE.LineBasicMaterial( { color : 0x0000ff } );
+var ellipseZ = new THREE.Line( geometry, material );
+gizmoCircles.push(ellipseZ);
+ellipseZ.applyMatrix(new THREE.Matrix4().makeRotationY(Math.PI/2));
+ellipseZ.layers.set(1);
+gizmo.add(ellipseZ);
+
+var geom = new THREE.Geometry();
+geom.vertices.push(new THREE.Vector3(0, 0, 0));
+geom.vertices.push(new THREE.Vector3(0, 1, 0));
+geom.vertices.push(new THREE.Vector3(0, 0, 0));
+geom.vertices.push(new THREE.Vector3(0, 1, 0));
+var startSeg = new THREE.LineSegments(geom, new THREE.LineBasicMaterial({
+  color: 0x000000
+}));
+startSeg.name = "startSeg";
+// gizmo.add(startSeg);
+
+var points_geom = new THREE.Geometry();
+points_geom.vertices.push(new THREE.Vector3(0,0,0));
+points_geom.vertices.push(new THREE.Vector3(0,0,0));
+var points = new THREE.Points( points_geom, new THREE.PointsMaterial({ color: 0x00ffff, size: 0.15 }) );
+// scene.add(points);
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshPhongMaterial( {color: 0x00ff00} );
+var cube = new THREE.Mesh( geometry, material );
+cube.applyMatrix(new THREE.Matrix4().makeTranslation(0,1,0));
+cube.matrixAutoUpdate = false;
+scene.add( cube );
+
+//angle arc
+var makeGizmoArc = function(from, to, isClockwise) {
+  let curve = new THREE.EllipseCurve(
+    0,  0,            // ax, aY
+    radius*0.9,  radius*0.9,  // xRadius, yRadius
+    from,  to,        // aStartAngle, aEndAngle
+    isClockwise,      // aClockwise
+    0                 // aRotation
+  );
+
+  let points = curve.getPoints( 50 );
+  let geometry = new THREE.BufferGeometry().setFromPoints( points );
+  let material = new THREE.LineBasicMaterial( { color : 0xff00ff } );
+
+  // Create the final object to add to the scene
+  let ellipse = new THREE.Line( geometry, material );
+  return ellipse;
+}
+// ==angle arc
+var gizmoArc;
+
+gizmo.applyMatrix(new THREE.Matrix4().makeTranslation(0, 1, 0));
+// gizmo.matrixAutoUpdate = false;
+scene.add(gizmo);
+
+var plane = new THREE.Plane(new THREE.Vector3(1, 1, 0.2), 8);
+// var helper = new THREE.PlaneHelper(plane, 8, 0xffff00);
+// scene.add( helper );
+
+var translationPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0);
+translationPlane.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,1,0), gizmo.position );
+var translationPlaneHelper = new THREE.PlaneHelper(this.translationPlane, 10, 0xff0000);
+// scene.add(translationPlaneHelper);
+var zeroAngleDir = new THREE.Vector3(1,0,0);
+
+var pickCircle = function(pos) {
+  raycaster.setFromCamera( pos, camera );
+
+  // cancel gizmo highlighing
+  for (let i=0; i<gizmoCircles.length; i++) {
+    let line = gizmoCircles[i];
+    if (line.material.prevColor) {
+      line.material.color.copy(line.material.prevColor);
+      line.material.needsUpdate = true;
+      delete line.material.prevColor;
+    }
+  }
+
+  raycaster.linePrecision = 0.15;
+
+  var intersects = raycaster.intersectObjects( gizmoCircles );
+  if (intersects.length > 0) {
+    let line = intersects[0].object;
+    if (!line.material.prevColor) {
+      line.material.prevColor = line.material.color.clone();
+      line.material.color.setHex(0xffff00);
+      line.material.needsUpdate = true;
+
+      if (line === ellipseX) {
+        zeroAngleDir.set(1,0,0);
+        translationPlane.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,1,0), gizmo.position );
+      } else if (line === ellipseY) {
+        zeroAngleDir.set(1,0,0);
+        translationPlane.setFromNormalAndCoplanarPoint ( new THREE.Vector3(0,0,1), gizmo.position );
+      } else if (line === ellipseZ) {
+        zeroAngleDir.set(0,0,-1);
+        translationPlane.setFromNormalAndCoplanarPoint ( new THREE.Vector3(1,0,0), gizmo.position );
+      }
+
+      controls.enabled = false;
+      return intersects[0].point;
+    }
+  } else {
+    controls.enabled = true;
+    return undefined;
+  }
+}
+
+var getPick = function(pos) {
+  raycaster.setFromCamera( pos, camera );
+  let target = new THREE.Vector3();
+  raycaster.ray.intersectPlane ( translationPlane, target );
+  return target;
+}
+
+var getAngle = function(v, zeroDir, normal) {
+  let angle = Math.acos(zeroDir.dot(v));
+
+  let adir = zeroDir.clone().cross(v).normalize().sub(normal);
+  if (adir.lengthSq() > 1e-3) {
+    angle = 2 * Math.PI - angle;
+  }
+
+  return angle;
+}
+
+var pickPoint0;
+var pickPoint1;
+
+var startAngle;
+var prevAngle;
+var endAngle;
+var totalRotation;
+
+var turnCount = 0;
+
+var mouse = new RayysMouse(renderer, camera);
+var raycaster = new THREE.Raycaster();
+mouse.subscribe(
+	function(pos) {
+    console.log(`Mouse down at: ${JSON.stringify(pos)}`);
+
+    pickPoint0 = pickCircle(pos);
+    if (pickPoint0) {
+
+      points.geometry.vertices[0].copy(pickPoint0);
+      points.geometry.verticesNeedUpdate = true;
+
+      startSeg.geometry.vertices[1].copy(pickPoint0.clone().sub(gizmo.position));
+      startSeg.geometry.verticesNeedUpdate = true;
+
+      let v = pickPoint0.clone().sub(gizmo.position).normalize();
+      startAngle = getAngle(v, zeroAngleDir, translationPlane.normal);
+      endAngle = startAngle;
+      prevAngle = startAngle;
+      totalRotation = 0.0;
+      turnCount = 0;
+
+      console.log(startAngle);
+    }
+  },
+  function(pos, sender) {
+    // console.log(`Mouse moved to: ${JSON.stringify(pos)}`);
+    if (!sender.mouseDown) {
+      pickCircle(pos);
+    } else if (pickPoint0 !== undefined) {
+      pickPoint1 = getPick(pos);
+
+      points.geometry.vertices[1].copy(pickPoint1);
+      points.geometry.verticesNeedUpdate = true;
+
+      startSeg.geometry.vertices[3].copy(pickPoint1.clone().sub(gizmo.position));
+      startSeg.geometry.verticesNeedUpdate = true;
+
+      let v = pickPoint1.clone().sub(gizmo.position).normalize();
+      prevAngle = endAngle;
+      endAngle = getAngle(v, zeroAngleDir, translationPlane.normal);
+
+      let angleIncrement = (endAngle - prevAngle);
+
+      if (angleIncrement < -Math.PI) {
+        angleIncrement += 2*Math.PI;
+        turnCount ++;
+      }
+
+      if (angleIncrement > Math.PI) {
+        angleIncrement -= 2*Math.PI;
+        turnCount --;
+      }
+
+      totalRotation += angleIncrement;
+      console.log(`turnCount=${turnCount}, rotation=${totalRotation}`);
+
+      if (gizmoArc) {
+        gizmo.remove(gizmoArc);
+      }
+      if (Math.abs(totalRotation) < 2*Math.PI) {
+        gizmoArc = makeGizmoArc(startAngle, endAngle, totalRotation < 0);
+      } else {
+        gizmoArc = makeGizmoArc(0, 2*Math.PI, false);
+      }
+      gizmoArc.lookAt(translationPlane.normal);
+      gizmo.add(gizmoArc);
+
+      let position = new THREE.Vector3();
+      let quaternion = new THREE.Quaternion();
+      let scale = new THREE.Vector3();
+      cube.matrix.decompose ( position, quaternion, scale );
+
+      let dquat = new THREE.Quaternion();
+      dquat.setFromAxisAngle( translationPlane.normal, angleIncrement );
+      quaternion.premultiply(dquat);
+      cube.matrix.compose ( position, quaternion, scale );
+    }
+  },
+  function(pos) {
+  	console.log(`Mouse up at: ${JSON.stringify(pos)}`);
+    if (gizmoArc) {
+      gizmo.remove(gizmoArc);
+    }
+  }
+);
+
+var animate = function() {
+  requestAnimationFrame(animate);
+  controls.update();
+
+  var dir = new THREE.Vector3();
+  camera.getWorldDirection(dir);
+  plane.setFromNormalAndCoplanarPoint(dir, origin);
+
+  var dist = plane.distanceToPoint(camera.position);
+  var f = Math.abs(dist) / 10;
+
+  var gposition = new THREE.Vector3();
+  var gquaternion = new THREE.Quaternion();
+  var gscale = new THREE.Vector3();
+  gizmo.matrix.decompose(gposition, gquaternion, gscale);
+  gizmo.matrix.identity();
+  gizmo.matrix.compose(gposition, gquaternion, new THREE.Vector3(f, f, f));
+
+  camera.layers.set(0);
+  renderer.render(scene, camera);
+  renderer.autoClearColor = false;
+  renderer.autoClearDepth = false;
+
+  camera.layers.set(2);
+  renderer.render(scene, camera);
+  renderer.autoClearColor = false;
+  renderer.autoClearDepth = false;
+
+	camera.layers.set(1);
+  renderer.render(scene, camera);
+  renderer.autoClearDepth = true;
+  renderer.autoClearColor = true;
+};
+
+animate();
