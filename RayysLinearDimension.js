33edb03e1806437066bd91cb5534d21c29d390b9
mmalex <horoshiloff@gmail.com>
2018-07-31 21:46:00 +0200

    fix missing camera reference, format js

--- RayysLinearDimension.js
+++ RayysLinearDimension.js
@@ -1,108 +1,108 @@
-class LinearDimension {
+class RayysLinearDimension {
 
-	constructor(domRoot, renderer, camera) {
-  	this.domRoot = domRoot;
-    this.renderer = renderer;
-    this.camera = camera;
+    constructor(domRoot, renderer, camera) {
+        this.domRoot = domRoot;
+        this.renderer = renderer;
+        this.camera = camera;
 
-		this.cb = {
-    	onChange: []
-    };
-  }
-
-  create(p0, p1, extrude) {
-
-		this.from = p0;
-    this.to = p1;
-    this.extrude = extrude;
-    
-  	this.node = new THREE.Object3D();
-    this.hidden = undefined;
-
-    let el = document.createElement("div");
-    el.id = this.node.id;
-    el.classList.add("dim");
-    el.style.left = "100px";
-    el.style.top = "100px";
-    el.innerHTML = "34.09";
-    this.domRoot.appendChild(el);
-    this.domElement = el;
-
-		this.update(camera);
-
-    return this.node;
-  }
-
-	update(camera) {
-  	this.camera = camera;
-
-		// re-create arrow
-    this.node.children.length = 0;
-
-    let p0 = this.from;
-    let p1 = this.to;
-    let extrude = this.extrude;
-
-		var pmin, pmax;
-		if (extrude.x >= 0 && extrude.y >= 0 && extrude.z >= 0) {
-       pmax = new THREE.Vector3(
-        extrude.x + Math.max(p0.x, p1.x),
-        extrude.y + Math.max(p0.y, p1.y),
-        extrude.z + Math.max(p0.z, p1.z));
-
-      pmin = new THREE.Vector3(
-        extrude.x < 1e-16 ? extrude.x + Math.min(p0.x, p1.x) : pmax.x,
-        extrude.y < 1e-16 ? extrude.y + Math.min(p0.y, p1.y) : pmax.y,
-        extrude.z < 1e-16 ? extrude.z + Math.min(p0.z, p1.z) : pmax.z);
-    } else if (extrude.x <= 0 && extrude.y <= 0 && extrude.z <= 0) {
-       pmax = new THREE.Vector3(
-        extrude.x + Math.min(p0.x, p1.x),
-        extrude.y + Math.min(p0.y, p1.y),
-        extrude.z + Math.min(p0.z, p1.z));
-
-      pmin = new THREE.Vector3(
-        extrude.x > -1e-16 ? extrude.x + Math.max(p0.x, p1.x) : pmax.x,
-        extrude.y > -1e-16 ? extrude.y + Math.max(p0.y, p1.y) : pmax.y,
-        extrude.z > -1e-16 ? extrude.z + Math.max(p0.z, p1.z) : pmax.z);
+        this.cb = {
+            onChange: []
+        };
     }
 
-		var origin = pmax.clone().add(pmin).multiplyScalar(0.5);
-    var dir = pmax.clone().sub(pmin);
-    dir.normalize();
+    create(p0, p1, extrude) {
 
-    var length = pmax.distanceTo(pmin)/2;
-    var hex = 0x0;
-    var arrowHelper0 = new THREE.ArrowHelper(dir, origin, length, hex, 0.5, 0.35);
-    this.node.add(arrowHelper0);
+        this.from = p0;
+        this.to = p1;
+        this.extrude = extrude;
 
-    dir.negate();
-    var arrowHelper1 = new THREE.ArrowHelper(dir, origin, length, hex, 0.5, 0.35);
-    this.node.add(arrowHelper1);
+        this.node = new THREE.Object3D();
+        this.hidden = undefined;
 
-		// reposition label
-    if (this.domElement !== undefined) {
-      let textPos = origin.project(this.camera);
-      // console.log(textPos);
+        let el = document.createElement("div");
+        el.id = this.node.id;
+        el.classList.add("dim");
+        el.style.left = "100px";
+        el.style.top = "100px";
+        el.innerHTML = "34.09";
+        this.domRoot.appendChild(el);
+        this.domElement = el;
 
-      let clientX = this.renderer.domElement.offsetWidth*(textPos.x + 1)/2 - 0.5 + this.renderer.domElement.offsetLeft;
+        this.update(this.camera);
 
-			let clientY = -this.renderer.domElement.offsetHeight*(textPos.y - 1)/2 - 0.5 + this.renderer.domElement.offsetTop;
-      
-      let dimWidth = this.domElement.offsetWidth;
-      let dimHeight = this.domElement.offsetHeight;
-
-      this.domElement.style.left = `${clientX - dimWidth/2}px`;
-      this.domElement.style.top  = `${clientY - dimHeight/2}px`;
-		}
-  }
-
-	detach() {
-  	if (this.node && this.node.parent) {
-    	this.node.parent.remove(this.node);
+        return this.node;
     }
-    if (this.domElement !== undefined) {
-      this.domRoot.removeChild(this.domElement);
-      this.domElement = undefined;
+
+    update(camera) {
+        this.camera = camera;
+
+        // re-create arrow
+        this.node.children.length = 0;
+
+        let p0 = this.from;
+        let p1 = this.to;
+        let extrude = this.extrude;
+
+        var pmin, pmax;
+        if (extrude.x >= 0 && extrude.y >= 0 && extrude.z >= 0) {
+            pmax = new THREE.Vector3(
+                extrude.x + Math.max(p0.x, p1.x),
+                extrude.y + Math.max(p0.y, p1.y),
+                extrude.z + Math.max(p0.z, p1.z));
+
+            pmin = new THREE.Vector3(
+                extrude.x < 1e-16 ? extrude.x + Math.min(p0.x, p1.x) : pmax.x,
+                extrude.y < 1e-16 ? extrude.y + Math.min(p0.y, p1.y) : pmax.y,
+                extrude.z < 1e-16 ? extrude.z + Math.min(p0.z, p1.z) : pmax.z);
+        } else if (extrude.x <= 0 && extrude.y <= 0 && extrude.z <= 0) {
+            pmax = new THREE.Vector3(
+                extrude.x + Math.min(p0.x, p1.x),
+                extrude.y + Math.min(p0.y, p1.y),
+                extrude.z + Math.min(p0.z, p1.z));
+
+            pmin = new THREE.Vector3(
+                extrude.x > -1e-16 ? extrude.x + Math.max(p0.x, p1.x) : pmax.x,
+                extrude.y > -1e-16 ? extrude.y + Math.max(p0.y, p1.y) : pmax.y,
+                extrude.z > -1e-16 ? extrude.z + Math.max(p0.z, p1.z) : pmax.z);
+        }
+
+        var origin = pmax.clone().add(pmin).multiplyScalar(0.5);
+        var dir = pmax.clone().sub(pmin);
+        dir.normalize();
+
+        var length = pmax.distanceTo(pmin) / 2;
+        var hex = 0x0;
+        var arrowHelper0 = new THREE.ArrowHelper(dir, origin, length, hex, 0.5, 0.35);
+        this.node.add(arrowHelper0);
+
+        dir.negate();
+        var arrowHelper1 = new THREE.ArrowHelper(dir, origin, length, hex, 0.5, 0.35);
+        this.node.add(arrowHelper1);
+
+        // reposition label
+        if (this.domElement !== undefined) {
+            let textPos = origin.project(this.camera);
+            // console.log(textPos);
+
+            let clientX = this.renderer.domElement.offsetWidth * (textPos.x + 1) / 2 - 0.5 + this.renderer.domElement.offsetLeft;
+
+            let clientY = -this.renderer.domElement.offsetHeight * (textPos.y - 1) / 2 - 0.5 + this.renderer.domElement.offsetTop;
+
+            let dimWidth = this.domElement.offsetWidth;
+            let dimHeight = this.domElement.offsetHeight;
+
+            this.domElement.style.left = `${clientX - dimWidth/2}px`;
+            this.domElement.style.top = `${clientY - dimHeight/2}px`;
+        }
     }
-  }
+
+    detach() {
+        if (this.node && this.node.parent) {
+            this.node.parent.remove(this.node);
+        }
+        if (this.domElement !== undefined) {
+            this.domRoot.removeChild(this.domElement);
+            this.domElement = undefined;
+        }
+    }
 }
